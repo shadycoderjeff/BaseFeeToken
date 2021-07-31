5b6201f7c4cd06f1aca3bc4153b8b7fcd9b44224
Jeff Smith <jeff@marlin.org>
2021-08-01 00:00:28 +0530

    add mint tests

--- test/basefeetoken.js
+++ test/basefeetoken.js
@@ -1,5 +1,21 @@
+const {
+  constants,    // Common constants, like the zero address and largest integers
+  expectEvent,  // Assertions for emitted events
+  expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
+
 const BaseFeeToken = artifacts.require("BaseFeeToken");
 
+
+contract("BaseFeeToken: initialize", accounts => {
+	it("should initialize properly", async () => {
+		let bft = await BaseFeeToken.deployed();
+		await bft.initialize("BaseFeeToken", "BFT", accounts[5]);
+		assert.equal(await bft.name(), "BaseFeeToken");
+		assert.equal(await bft.symbol(), "BFT");
+		assert.isTrue(await bft.hasRole(constants.ZERO_BYTES32, accounts[5]));
+	});
+});
 
 contract("BaseFeeToken: supportsInterface", accounts => {
 	it("should return false for 0xffffffff", async () => {
@@ -18,13 +34,23 @@ contract("BaseFeeToken: supportsInterface", accounts => {
 	});
 });
 
-contract("BaseFeeToken: initialize", accounts => {
-	it("should initialize properly", async () => {
+contract("BaseFeeToken: mint", accounts => {
+	it("should mint only when minter role", async () => {
 		let bft = await BaseFeeToken.deployed();
 		await bft.initialize("BaseFeeToken", "BFT", accounts[5]);
-		assert.equal(await bft.name(), "BaseFeeToken");
-		assert.equal(await bft.symbol(), "BFT");
-		assert.isTrue(await bft.hasRole("0x0000000000000000000000000000000000000000000000000000000000000000", accounts[5]));
+		await bft.grantRole(web3.utils.sha3("MINTER_ROLE"), accounts[3], {from: accounts[5]});
+
+		expectRevert(
+			bft.mint(accounts[4], 1000, {from: accounts[5]}),
+			"BFT: must have minter role to mint",
+		);
+
+		await bft.mint(accounts[4], 1000, {from: accounts[3]});
+
+		assert.equal(
+			await bft.balanceOf(accounts[4]),
+			1000
+		);
 	});
 });
 
